42e5750655c6115eb78354ec43b454955e4b6d16
Man Mai <42661871+maimand@users.noreply.github.com>
2023-07-27 14:15:27 +0700

    - App: Refactor app repository

--- lib/app/data/services/auth_service.dart
+++ lib/app/data/services/auth_service.dart
@@ -1,5 +1,32 @@
+import 'package:clone_notes/app/data/models/user_model.dart';
+import 'package:clone_notes/app/data/repository/user_repository.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AuthService extends GetxService {
-  final String uid = 'man-test';
+  final UserRepository userRepository;
+
+  String uid = 'man-test';
+
+  AuthService(this.userRepository);
+
+  Future<void> registerUser(
+      {required String username, required String password}) {
+    return userRepository.register(username, hashPassword(password));
+  }
+
+  Future<UserModel?> login(
+      {required String username, required String password}) async {
+    try {
+      final user = userRepository.login(username, hashPassword(password));
+      return user;
+    } on Exception catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
+  }
+
+  String hashPassword(String password) {
+    return '${password}secretKey';
+  }
 }
